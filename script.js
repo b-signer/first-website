03f8071e7cdc7c587ff34598abf2efa0fd776305
b-signer <bennysigner@gmail.com>
2025-08-25 11:29:02 -0700

    Project site init

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const btn = document.getElementById('btn');
+const msg = document.getElementById('msg');
+btn.addEventListener('click', () => {
+  msg.textContent = "🎉 It works!";
+});
