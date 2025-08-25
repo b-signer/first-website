38a3e62eddf069e1bcdf9f77c0cb2a4e31b3d084
b-signer <bennysigner@gmail.com>
2025-08-25 11:55:35 -0700

    Added a second section and a second button

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const msg = document.getElementById('msg');
 btn.addEventListener('click', () => {
   msg.textContent = "The Button Works!";
 });
+
+const btn2 = document.getElementById('btn2');
+const msg2 = document.getElementById('msg2');
+btn2.addEventListener('click', () => {
+    msg.textContent = "The second button also works!";
+});
